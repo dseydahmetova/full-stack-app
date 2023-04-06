c389a7a37044a37126a6e3d36a54fb8718514c1b
dseydahmetova <dseydahmetova@gmail.com>
2023-04-06 05:02:32 -0400

    6 attempt to fix the render deployment

--- backend/controllers/placeController.js
+++ backend/controllers/placeController.js
@@ -6,50 +6,50 @@ const axios = require('axios')
 module.exports.seed = async (req, res) => {
 
     const key = process.env.REACT_APP_API_KEY
-    try{
+    try {
         const response = await axios.get(
             `https://developer.nps.gov/api/v1/parks?&api_key=${key}`
         );
-       
+
         const myPlace = response.data.data
-     for(let i=0; i< myPlace.length; i++){
-     
-                 const places = new Place({
-                    image: myPlace[i].images[0]['url'],
-                    fullName: myPlace[i].fullName,
-                    address: myPlace[i].addresses[0]['line1'],
-                    city: myPlace[i].addresses[0]['city'],
-                    stateCode: myPlace[i].addresses[0]['stateCode'],
-                    description: myPlace[i].description,
-                    weatherInfo: myPlace[i].weatherInfo,
-                    user: 'admin'
-                 })
-                 await places.save()
-             }
-        
-    }catch(error){
+        for (let i = 0; i < myPlace.length; i++) {
+
+            const places = new Place({
+                image: myPlace[i].images[0]['url'],
+                fullName: myPlace[i].fullName,
+                address: myPlace[i].addresses[0]['line1'],
+                city: myPlace[i].addresses[0]['city'],
+                stateCode: myPlace[i].addresses[0]['stateCode'],
+                description: myPlace[i].description,
+                weatherInfo: myPlace[i].weatherInfo,
+                user: 'admin'
+            })
+            await places.save()
+        }
+
+    } catch (error) {
         console.log(error)
     }
 }
 
 // get all places from DB
 module.exports.index = async (req, res) => {
-    const {page} = req.query
+    const { page } = req.query
     try {
         const LIMIT = 9;
         // get the starting index of every page
         const startIndex = (Number(page) - 1) * LIMIT
-        // const total = await Place.countDocuments({}).maxTimeMS(30000)
-//id = -1 the latest post, limit per page and skip previos pages
-        // const places = await Place.find().sort({_id: -1}).limit(LIMIT).skip(startIndex)
-        // res.status(200).json({data: places, currentPage: Number(page), totalnumberOfPages: Math.ceil(total / LIMIT)})
-  
+        const total = await Place.countDocuments({}).maxTimeMS(50000)
+        // id = -1 the latest post, limit per page and skip previos pages
+        const places = await Place.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex)
+        res.status(200).json({ data: places, currentPage: Number(page), totalnumberOfPages: Math.ceil(total / LIMIT) })
 
- const places = await Place.find({}, { maxTimeMS: 5000 })
-        res.status(200).json({data: places})
-  
 
-    } catch(err) {
+        //  const places = await Place.find({}, { maxTimeMS: 5000 })
+        //         res.status(200).json({data: places})
+
+
+    } catch (err) {
         res.status(400).json({ error: err.message })
     }
 }
@@ -59,7 +59,7 @@ module.exports.show = async (req, res) => {
     try {
         const place = await Place.findById(req.params.id)
         res.status(200).json(place)
-    } catch(err) {
+    } catch (err) {
         res.status(404).json({ error: err.message })
     }
 }
@@ -69,7 +69,7 @@ module.exports.create = async (req, res) => {
     try {
         const place = await Place.create(req.body)
         res.status(200).json(place)
-    } catch(err) {
+    } catch (err) {
         res.status(400).json({ error: err.message })
     }
 }
@@ -79,7 +79,7 @@ module.exports.update = async (req, res) => {
     try {
         const updatedPlace = await Place.findByIdAndUpdate(req.params.id, req.body, { new: true })
         res.status(200).json(updatedPlace)
-    } catch(err) {
+    } catch (err) {
         res.status(400).json({ error: err.message })
     }
 }
@@ -89,7 +89,7 @@ module.exports.delete = async (req, res) => {
     try {
         await Place.findByIdAndDelete(req.params.id)
         res.status(200).json({ message: 'deleted successfully' })
-    } catch(err) {
+    } catch (err) {
         res.status(400).json({ error: err.message })
     }
 }
@@ -97,10 +97,10 @@ module.exports.delete = async (req, res) => {
 //count the likes for certain place
 module.exports.like = async (req, res) => {
     try {
-        const place = await Place.findById(req.params.id) 
-        const updatedPlace = await Place.findByIdAndUpdate(req.params.id, {likeCount: place.likeCount + 1}, { new: true })
+        const place = await Place.findById(req.params.id)
+        const updatedPlace = await Place.findByIdAndUpdate(req.params.id, { likeCount: place.likeCount + 1 }, { new: true })
         res.status(200).json(updatedPlace)
-    } catch(err) {
+    } catch (err) {
         res.status(400).json({ error: err.message })
     }
 }
@@ -111,12 +111,12 @@ module.exports.save = async (req, res) => {
     try {
         const place = await Place.findById(req.body.placeId)
         const user = await User.findByIdAndUpdate(req.body.userId, {
-            $push:{
-                        savedPlaces: place._id
-                    }
+            $push: {
+                savedPlaces: place._id
+            }
         })
-        res.status(200).json({savedPlaces: user.savedPlaces})
-    } catch(err) {
+        res.status(200).json({ savedPlaces: user.savedPlaces })
+    } catch (err) {
         res.status(404).json({ error: err.message })
     }
 }
@@ -127,8 +127,8 @@ module.exports.save = async (req, res) => {
 module.exports.showSavedPlaces = async (req, res) => {
     try {
         const user = await User.findById(req.params.id).populate('savedPlaces')
-        res.status(200).json({savedPlaces: user.savedPlaces})
-    } catch(err) {
+        res.status(200).json({ savedPlaces: user.savedPlaces })
+    } catch (err) {
         res.status(404).json({ error: err.message })
     }
 }
@@ -142,8 +142,8 @@ module.exports.deleteSavedPlaces = async (req, res) => {
                 savedPlaces: req.params.placeId
             }
         })
-        res.status(200).json({message: 'deleted successfully'})
-    } catch(err) {
+        res.status(200).json({ message: 'deleted successfully' })
+    } catch (err) {
         res.status(404).json({ error: err.message })
     }
 }
@@ -154,13 +154,13 @@ module.exports.deleteSavedPlaces = async (req, res) => {
 //query ->  /places?page=1 -> page = 1
 
 module.exports.search = async (req, res) => {
-   const {searchQuery} = req.query
-   try {
+    const { searchQuery } = req.query
+    try {
         // i used to lowercase search query  
-               const fullName = new RegExp(searchQuery, 'i') 
-          const place = await Place.find({fullName})    
-        res.status(200).json({place})
-    } catch(err) {
+        const fullName = new RegExp(searchQuery, 'i')
+        const place = await Place.find({ fullName })
+        res.status(200).json({ place })
+    } catch (err) {
         res.status(404).json({ error: err.message })
     }
 }
@@ -169,9 +169,9 @@ module.exports.search = async (req, res) => {
 module.exports.getSavedPlaces = async (req, res) => {
     try {
         const user = await User.findById(req.params.id)
-       
-        res.status(200).json({savedPlaces: user?.savedPlaces})
-    } catch(err) {
+
+        res.status(200).json({ savedPlaces: user?.savedPlaces })
+    } catch (err) {
         res.status(404).json({ error: err.message })
     }
 }
